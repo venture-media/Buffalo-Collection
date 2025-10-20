0165a001af782c25511c896608df9b72d033ffb3
Leon <150437885+Leon2332@users.noreply.github.com>
2025-10-20 15:49:57 +0200

    Delete generatepress-child/includes/08-favicon.php

--- generatepress-child/includes/08-favicon.php
+++
@@ -1,5 +0,0 @@
-<?php
-function custom_svg_favicon() {
-    echo '<link rel="icon" href="' . get_stylesheet_directory_uri() . '/favicon.svg" type="image/svg+xml">';
-}
-add_action('wp_head', 'custom_svg_favicon');
